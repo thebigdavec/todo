27d5146c49766b8ff5527073a833c913e6c02545
thebigdavec <daverbcollison@gmail.com>
2020-08-16 10:10:42 +0100

    Add a space to the H1 element

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <h1>TodoList</h1>
+    <h1>Todo List</h1>
     <div id="nav">
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
